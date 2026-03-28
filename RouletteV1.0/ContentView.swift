f163fe12b4666696ae82c8b29aa6351cb9baf153
Glen O'Connor <glenoconnor2026@icloud.com>
2026-03-28 18:23:05 +1100

    Initial Commit

---
+++ RouletteV1.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RouletteV1.0
+//
+//  Created by Glen O'Connor on 28/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RouletteV1.0/RouletteV1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  RouletteV1_0App.swift
+//  RouletteV1.0
+//
+//  Created by Glen O'Connor on 28/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct RouletteV1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
